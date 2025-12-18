c80cdb5a4bf028ebfd4649f7123a6981fbdf461c
johnAndPython <barthos@gmx.de>
2025-12-18 19:03:39 +0100

    added cls_ball.py file

---
+++ cls_ball.py
@@ -0,0 +1,16 @@
+import pygame
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.surface = pygame.Surface((20, 20))
+        self.surface.fill((255, 100, 100))
+        self.rect = self.surface.get_rect()
+
+    def update(self, *args, **kwargs):
+        pass
+        #return super().update(*args, **kwargs)
+
+    def draw(self, surface: pygame.Surface) -> None:
+        surface.blit(self.surface, self.rect)

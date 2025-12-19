76fc948b22e28b42a56ea08eba402f63ca6be404
johnAndPython <barthos@gmx.de>
2025-12-19 15:50:51 +0100

    added attrubutes to class Bloc

--- cls_block.py
+++ cls_block.py
@@ -1,11 +1,19 @@
 import pygame
 
 class Block(pygame.sprite.Sprite):
-    def __init__(self, *groups) -> None:
-        super().__init__(*groups)
+    def __init__(self, left: int, top: int, width: int, height: int, color: tuple[int]) -> None:
+        super().__init__()
 
-        self.surface = None
-        self.rect = None
+        self.width = width
+        self.height = height
+        self.color = color
+
+        self.image = pygame.Surface((self.width, self.height))
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+
+        self.rect.left = left
+        self.rect.top = top
 
     def update(self) -> None:
         pass
